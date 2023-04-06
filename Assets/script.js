d87bb89c673eea95ab5af971d6615084c18184e3
Matt Miceli <126421060+Matty-Bamboo@users.noreply.github.com>
2023-04-05 17:40:16 -0700

    further udpates to the javascript

--- Assets/script.js
+++ Assets/script.js
@@ -11,41 +11,43 @@
 var generateBtn = document.querySelector("#generate");
 
 // characters available for random password
+var length = "";
 var number = '1234567890';
 var alphaLower = 'abcdefghijklmnopqrstuvwxyz';
 var alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var specialChar = '!#&$%&@/<>?~';
 
+// user confirmation of user selection variables
 var confirmNumber
 var confirmUppercase
-var confirmLowercase
+var confirmLowercase 
 var confirmSpecChar
 
+// convert string to array
 var newalpahArray = alphaUpper.split("")
 var newlowerArray = alphaLower.split("")
 var newspecCharArray = specialChar.split("")
 var newnumberArray = number.split("")
 
+charArray =[];
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword,);
 
-
-var length = '';
-
 // Deterining the users selections for password
 function userSelection() {
 length = prompt("Choose the length of your password between 8 - 128 characters");
   if (length < 8 || length > 128 || length == null) {
-    return alert("Please choose a number between 8 - 128");
+    alert("Please choose a number between 8 - 128");
+    return;
 }  
 if (length >=8 || length <=128) {
     console.log(length);
@@ -57,53 +59,57 @@ if (length >=8 || length <=128) {
     console.log(confirmLowercase);
     confirmSpecChar = confirm ("Do you want your password to have special characters?");
     console.log(confirmSpecChar);
+    
  if (confirmNumber === false && confirmUppercase === false && confirmLowercase === false && confirmSpecialChar === false) 
     return alert("At least one character type must be selected");
-
  }
-// record console log values 
-var userAnswer = {
-  length : length,
-  number: confirmNumber,
-  alphaUpper: confirmUppercase,
-  alphaLower: confirmLowercase,
-  specialChar: confirmSpecChar,
-  }
-  return userAnswer;
 }
 
 function getInfo() {
   console.log(confirmNumber);
-  console.log(confirmUppercase);
-  console.log(confirmLowercase);
-  console.log(confirmSpecChar);
-  if (confirmUppercase) {
-    console.log(newalpahArray)
-    }
-  if (confirmLowercase) {
-    console.log(newlowerArray)
-  }
-  if (confirmSpecChar) {
-    console.log(newspecCharArray)
-  }
   if (confirmNumber) {
-    console.log(newnumberArray)
+    console.log(newnumberArray);
+    }
+  console.log(confirmUppercase); 
+  if (confirmUppercase) {
+    console.log(newalpahArray);
+    }
+  console.log(confirmLowercase);
+  if (confirmLowercase) {
+    console.log(newlowerArray);
+    }
+  console.log(confirmSpecChar);
+  if (confirmSpecChar) {
+    console.log(newspecCharArray);
+    }
+  
   }
  
+function CharacterSet() {
+  charArray = [];
+  length =console.log(length);
+ if (newalpahArray) {
+    charArray = charArray.concat(newalpahArray);
+  }
+  if (newlowerArray) {
+    charArray = charArray.concat(newlowerArray);
+  }
+  if (newspecCharArray) {
+    charArray = charArray.concat(newspecCharArray);
+  }
+ if (newnumberArray) {
+   charArray = charArray.concat(newnumberArray);
+  }
+  return charArray
 }
 
-
-var pwd = '';
-document.getElementById("password").innerHTML = pwd;
-
 function generatePassword() {
-    
-    var userInfo = userSelection();
-    console.log(userInfo);
-    var characters = (number + alphaLower + alphaUpper + specialChar);
-    
-    for (var i = 0; i < 1; ++i) {
-      pwd += characters.charAt(Math.floor(Math.random() * userInfo.length));
+  var password = "";
+  var length = "";
+  var charArray = "";
+  for (let index = 0; index < length; index++) {
+    var randomIndex = Math.floor(Math.random() * charArray.length);
+    password = password + charArray[randomIndex]
   }
-  return pwd;
+  return password
 }
